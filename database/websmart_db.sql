75b3246ae8527c9ec90cab60961ac99d14cf0f17
Sam120300 <shingsam03@gmail.com>
2024-10-09 22:01:40 +0800

    final commit

--- database/websmart_db.sql
+++ database/websmart_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 07, 2024 at 03:23 PM
+-- Generation Time: Oct 09, 2024 at 04:00 PM
 -- Server version: 10.4.19-MariaDB
 -- PHP Version: 8.0.7
 
@@ -46,36 +46,6 @@ CREATE TABLE `archive_list` (
   `search_count` int(8) NOT NULL DEFAULT 0,
   `submitted_by` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `archive_list`
---
-
-INSERT INTO `archive_list` (`id`, `curriculum_id`, `year`, `title`, `type`, `style`, `abstract`, `members`, `banner_path`, `document_path`, `status`, `student_id`, `date_created`, `date_updated`, `views_count`, `search_count`, `submitted_by`) VALUES
-(1, 26, 2024, 'Enhancing Cybersecurity with Blockchain Technology: A New Approach to Data Privacy', 0, 0, 'uploads/abstracts/abstract-1.pdf?v=1726617285', '&lt;p&gt;123&lt;/p&gt;', '', 'uploads/pdf/document-1.pdf?v=1726617285', 1, NULL, '2024-09-18 07:54:45', '2024-09-29 15:39:56', 57, 644, 'PNC Administrator'),
-(2, 26, 2023, 'AI-Driven Solutions for Predicting Software Defects in Agile Development', 0, 0, 'uploads/abstracts/abstract-2.pdf?v=1726617309', '&lt;p&gt;123&lt;/p&gt;', '', 'uploads/pdf/document-2.pdf?v=1726617309', 0, NULL, '2024-09-18 07:55:09', '2024-09-18 08:45:06', 2, 140, 'PNC Administrator'),
-(3, 26, 2022, 'Developing an Adaptive Algorithm for Real-Time Traffic Management in Smart Cities', 0, 0, 'uploads/abstracts/abstract-3.pdf?v=1726617332', '&lt;p&gt;123&lt;/p&gt;', '', 'uploads/pdf/document-3.pdf?v=1726617332', 0, NULL, '2024-09-18 07:55:32', '2024-09-20 22:06:02', 3, 140, 'PNC Administrator'),
-(4, 34, 2021, 'The Impact of Urbanization on Biodiversity in Coastal Ecosystems', 0, 0, 'uploads/abstracts/abstract-4.pdf?v=1726617368', '&lt;p&gt;123&lt;/p&gt;', '', 'uploads/pdf/document-4.pdf?v=1726617368', 1, NULL, '2024-09-18 07:56:08', '2024-09-29 15:39:56', 2, 256, 'PNC Administrator'),
-(5, 21, 2019, 'Assessing the Potential of Solar Energy in Reducing Carbon Emissions in Developing Countries', 0, 0, 'uploads/abstracts/abstract-5.pdf?v=1726617389', '&lt;p&gt;123&lt;/p&gt;', '', 'uploads/pdf/document-5.pdf?v=1726617389', 1, NULL, '2024-09-18 07:56:29', '2024-09-29 15:39:56', 2, 408, 'PNC Administrator'),
-(6, 31, 2019, 'Microplastic Pollution: Long-Term Effects on Marine Life and Mitigation Strategies', 0, 0, 'uploads/abstracts/abstract-6.pdf?v=1726617406', '&lt;p&gt;123&lt;/p&gt;', '', 'uploads/pdf/document-6.pdf?v=1726617406', 1, NULL, '2024-09-18 07:56:46', '2024-09-29 15:39:56', 11, 396, 'PNC Administrator'),
-(7, 21, 2014, 'The Influence of Social Media on Adolescent Self-Esteem and Anxiety', 0, 0, 'uploads/abstracts/abstract-7.pdf?v=1726617435', '&lt;p&gt;123&lt;/p&gt;', '', 'uploads/pdf/document-7.pdf?v=1726617435', 1, NULL, '2024-09-18 07:57:15', '2024-09-29 15:39:56', 2, 390, 'PNC Administrator'),
-(8, 21, 2017, 'Cognitive Behavioral Therapy for Managing Anxiety in Children: A Comparative Study', 0, 0, 'uploads/abstracts/abstract-8.pdf?v=1726617464', '&lt;p&gt;123&lt;/p&gt;', '', 'uploads/pdf/document-8.pdf?v=1726617464', 0, NULL, '2024-09-18 07:57:44', '2024-09-21 22:38:49', 14, 140, 'PNC Administrator'),
-(9, 21, 2020, 'Emotional Intelligence as a Predictor of Job Satisfaction Among Healthcare Workers', 0, 0, 'uploads/abstracts/abstract-9.pdf?v=1726617486', '&lt;p&gt;123&lt;/p&gt;', '', 'uploads/pdf/document-9.pdf?v=1726617486', 1, NULL, '2024-09-18 07:58:06', '2024-09-29 15:39:56', 2, 410, 'PNC Administrator'),
-(10, 23, 2016, 'The Role of Fintech in Expanding Financial Inclusion in Sub-Saharan Africa', 0, 0, 'uploads/abstracts/abstract-10.pdf?v=1726617508', '&lt;p&gt;123&lt;/p&gt;', '', 'uploads/pdf/document-10.pdf?v=1726617508', 1, NULL, '2024-09-18 07:58:28', '2024-09-29 15:39:56', 2, 358, 'PNC Administrator'),
-(11, 24, 2015, 'Sustainable Business Practices and their Impact on Consumer Perception and Loyalty', 0, 0, 'uploads/abstracts/abstract-11.pdf?v=1726617527', '&lt;p&gt;123&lt;/p&gt;', '', 'uploads/pdf/document-11.pdf?v=1726617527', 1, NULL, '2024-09-18 07:58:47', '2024-09-29 15:39:56', 2, 274, 'PNC Administrator'),
-(12, 24, 2019, 'Cryptocurrency Adoption: Implications for Traditional Banking Systems', 0, 0, 'uploads/abstracts/abstract-12.pdf?v=1726617546', '&lt;p&gt;123&lt;/p&gt;', '', 'uploads/pdf/document-12.pdf?v=1726617546', 1, NULL, '2024-09-18 07:59:06', '2024-09-29 15:39:56', 2, 445, 'PNC Administrator'),
-(13, 28, 2021, 'Gamification in Education: Enhancing Student Engagement in Online Learning', 0, 0, 'uploads/abstracts/abstract-13.pdf?v=1726617567', '&lt;p&gt;123&lt;/p&gt;', '', 'uploads/pdf/document-13.pdf?v=1726617567', 1, NULL, '2024-09-18 07:59:27', '2024-09-29 15:39:56', 2, 272, 'PNC Administrator'),
-(14, 30, 2016, 'Teacher Burnout: Causes, Consequences, and Strategies for Prevention', 0, 0, 'uploads/abstracts/abstract-14.pdf?v=1726617586', '&lt;p&gt;123&lt;/p&gt;', '', 'uploads/pdf/document-14.pdf?v=1726617586', 1, NULL, '2024-09-18 07:59:46', '2024-09-29 15:39:56', 4, 452, 'PNC Administrator'),
-(15, 29, 2021, 'Inclusive Education for Students with Disabilities: Evaluating Policy Implementation', 0, 0, 'uploads/abstracts/abstract-15.pdf?v=1726617601', '&lt;p&gt;123&lt;/p&gt;', '', 'uploads/pdf/document-15.pdf?v=1726617601', 1, NULL, '2024-09-18 08:00:01', '2024-09-29 15:39:56', 5, 369, 'PNC Administrator'),
-(16, 31, 2024, 'Telemedicine: Evaluating Its Effectiveness in Rural Healthcare Delivery', 0, 0, 'uploads/abstracts/abstract-16.pdf?v=1726617620', '&lt;p&gt;123&lt;/p&gt;', '', 'uploads/pdf/document-16.pdf?v=1726617620', 1, NULL, '2024-09-18 08:00:20', '2024-09-29 15:39:56', 2, 357, 'PNC Administrator'),
-(17, 32, 2016, 'Exploring the Role of Gut Microbiota in the Development of Type 2 Diabetes', 0, 0, 'uploads/abstracts/abstract-17.pdf?v=1726617704', '&lt;p&gt;123&lt;/p&gt;', '', 'uploads/pdf/document-17.pdf?v=1726617704', 1, NULL, '2024-09-18 08:01:44', '2024-09-29 15:39:56', 2, 370, 'PNC Administrator'),
-(18, 26, 2022, 'The Long-Term Psychological Impact of COVID-19 on Frontline Healthcare Workers', 0, 0, 'uploads/abstracts/abstract-18.pdf?v=1726617726', '&lt;p&gt;123&lt;/p&gt;', '', 'uploads/pdf/document-18.pdf?v=1726617726', 1, NULL, '2024-09-18 08:02:06', '2024-09-29 15:39:56', 2, 357, 'PNC Administrator'),
-(20, 34, 2015, '3D Printing in Aerospace: Advancements and Challenges in Structural Design', 0, 0, 'uploads/abstracts/abstract-20.pdf?v=1726617767', '&lt;p&gt;123&lt;/p&gt;', '', 'uploads/pdf/document-20.pdf?v=1726617767', 1, NULL, '2024-09-18 08:02:47', '2024-09-29 15:39:56', 2, 278, 'PNC Administrator'),
-(21, 32, 2020, 'asda', 0, 0, '', '&lt;p&gt;asd&lt;/p&gt;', '', '', 0, NULL, '2024-09-20 21:13:41', NULL, 0, 0, 'CHRISTIAN SAMONTE'),
-(22, 29, 2020, 'asdasda', 0, 0, 'uploads/abstracts/abstract-22.pdf?v=1726838064', '&lt;p&gt;asdadsas&lt;/p&gt;', '', 'uploads/pdf/document-22.pdf?v=1726838064', 0, NULL, '2024-09-20 21:14:24', '2024-09-20 21:14:24', 1, 0, 'CHRISTIAN SAMONTE'),
-(24, 23, 2024, 'Test', 6, 4, 'uploads/abstracts/abstract-24.pdf?v=1726898268', '&lt;p&gt;test&lt;/p&gt;', '', 'uploads/pdf/document-24.pdf?v=1726898268', 1, NULL, '2024-09-21 13:57:48', '2024-09-29 15:39:56', 3, 22, 'admin babols'),
-(26, 27, 2024, 'sample', 8, 4, 'uploads/abstracts/abstract-26.pdf?v=1726906002', '&lt;p&gt;sample&lt;br&gt;&lt;/p&gt;', '', 'uploads/pdf/document-26.pdf?v=1726906002', 1, NULL, '2024-09-21 16:06:42', '2024-09-29 15:39:32', 2, 19, 'PNC Administrator'),
-(27, 32, 2021, 'asdasdasdasd', 9, 2, 'uploads/abstracts/abstract-27.pdf?v=1727882281', '&lt;p&gt;adasdasd&lt;/p&gt;', '', 'uploads/pdf/document-27.pdf?v=1727882281', 0, NULL, '2024-10-02 23:18:01', '2024-10-02 23:18:01', 1, 0, 'PNC Administrator');
 
 -- --------------------------------------------------------
 
@@ -245,15 +215,6 @@ CREATE TABLE `student_list` (
   `type` int(11) DEFAULT 1
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Dumping data for table `student_list`
---
-
-INSERT INTO `student_list` (`id`, `firstname`, `middlename`, `lastname`, `department_id`, `curriculum_id`, `student_id`, `user_type`, `password`, `gender`, `status`, `avatar`, `date_created`, `date_updated`, `type`) VALUES
-(22, 'Sample', '', 'Student', 12, 23, '1-200101', 'student', '21232f297a57a5a743894a0e4a801fc3', 'Female', 1, 'uploads/students/student-22.png?v=1725722384', '2024-08-18 21:57:14', '2024-09-20 20:36:48', 1),
-(30, 'CHRISTIAN', 'AMISTOSO', 'SAMONTE', 13, 26, '123', 'student', '202cb962ac59075b964b07152d234b70', 'Male', 1, '', '2024-09-20 20:48:32', '2024-09-20 20:48:49', 1),
-(31, 'CHRISTIAN', 'AMISTOSO', 'SAMONTE', 13, 26, '120300', 'student', '7bbeb82f3733123b3758d0f25078e3fc', 'Male', 1, '', '2024-09-20 23:47:42', '2024-09-20 23:50:37', 1);
-
 -- --------------------------------------------------------
 
 --
@@ -309,10 +270,7 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `firstname`, `middlename`, `lastname`, `username`, `password`, `avatar`, `last_login`, `type`, `status`, `date_added`, `date_updated`) VALUES
-(1, 'PNC', NULL, 'Administrator', 'admin', '21232f297a57a5a743894a0e4a801fc3', 'uploads/admins/admin-1.png?v=1725722350', NULL, 2, 1, '2021-01-20 14:02:37', '2024-09-18 20:45:43'),
-(9, 'admin', NULL, 'blosom', 'blosom', '22570ae75af72d1fc2f26ddb8a87c477', 'uploads/admins/admin-9.png?v=1723955560', NULL, 3, 1, '2024-08-18 12:32:40', '2024-09-18 20:46:40'),
-(10, 'admin', NULL, 'babols', 'babols', '8150104451e63753827d79a59d30d838', 'uploads/admins/admin-10.png?v=1723955577', NULL, 3, 1, '2024-08-18 12:32:57', '2024-09-18 20:46:38'),
-(11, 'admin', NULL, 'baterkap', 'baterkap', 'd403e479520f6d402f96d658ba673bf3', 'uploads/admins/admin-11.png?v=1723955591', NULL, 3, 1, '2024-08-18 12:33:11', '2024-09-18 20:46:35');
+(1, 'PNC', NULL, 'Administrator', 'admin', '21232f297a57a5a743894a0e4a801fc3', 'uploads/admins/admin-1.png?v=1728481960', NULL, 2, 1, '2021-01-20 14:02:37', '2024-10-09 21:52:40');
 
 --
 -- Indexes for dumped tables
@@ -375,7 +333,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `archive_list`
 --
 ALTER TABLE `archive_list`
-  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `curriculum_list`
@@ -393,13 +351,13 @@ ALTER TABLE `department_list`
 -- AUTO_INCREMENT for table `forms_list`
 --
 ALTER TABLE `forms_list`
-  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `student_list`
 --
 ALTER TABLE `student_list`
-  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=32;
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `system_info`
